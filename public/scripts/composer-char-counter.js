6b316075707580cb9d6616d9ce7535be6b356fd3
Justinette2175 <jugagnepain75@gmail.com>
2017-01-24 18:09:04 +0000

    made counter red if over 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,14 @@ $(document).ready(function(){
   $(".new-tweet form textarea").on('keyup', function(){
     var length = $(this).val().length;
     var remaining = 140 - length;
-    $(this).closest(".new-tweet").find(".counter").text(remaining);
+    var $counter = $(this).closest(".new-tweet").find(".counter");
+    $counter.text(remaining);
+    if (length > 140){
+      $counter.addClass("over-length");
+    }
+    else if (length <= 140){
+      $counter.removeClass("over-length");
+    }
   })
 
 
